842df67bcbb876b5c7067e861d33d9e5d2ccde34
Jure Menart <juremenart@gmail.com>
2017-08-28 05:25:57 +0200

    update

--- uwb_gen_pulse.m
+++ uwb_gen_pulse.m
@@ -1,37 +1,28 @@
-% Generate pulse of type p_type of duration p_len with sampling frequency f_s
-%
-% p_type can be:
-%   0 - normal Gaussian pulse
-%   1 - 1st order Gauss derivate
-%   2 - 2nd order Gauss derivate
-%   3 - sinus
-function out_pulse=uwb_gen_pulse(p_type, p_len, f_s, dbg_level)
+%% Generate pulse of type p_type of duration p_len with sampling frequency f_s
+%%
+%% p_type can be:
+%%   0 - normal Gaussian pulse
+%%   1 - 1st order Gauss derivate
+%%   2 - 2nd order Gauss derivate
+%%   3 - sinus
+function out_pulse=uwb_gen_pulse(p_type, p_len, f_s)
 
-  max_p_len=20e-9;
+  p_window=2*p_len; % Pulse window double the pulse length
 
-  if(p_type >= 0 && p_type <= 2)
+  %% Be sure to always center it around 0
+  s_p=[[0:-2/(p_window*f_s):-1],[0+2/(p_window*f_s):2/(p_window*f_s):+1-2/(p_window*f_s)]];
+  s_p=sort(s_p);
+
+
+  if(p_type == 0)
+    %% Gaussian functions parameters:
+    %% mu always equals 0 (no shift around 0)
+    %% variance (sigma^2) depends on the pulse length 
+    sigma=0.2;
+    out_pulse=(1/(sigma*sqrt(2*pi))).*exp(-s_p.^2/(sigma^2*2)); % generate single Gauss
   end
-  if(p_type == 3)
-    s_p=-pi:(2*pi)/(f_s*p_len):+pi
-  end
-  
-  % Gaussian functions parameters:
-  % mu always equals 0 (no shift around 0)
-  % variance (sigma^2) depends on the pulse length 
-  s_p=-5:10/(max_p_len*2*f_s):+5-1/(max_p_len*2*f_s);
-  load('sigma_poly.mat')
-  sigma=p_len*1e9*sigma_poly(1) + sigma_poly(2)
-  out_pulse=(1/(sigma*sqrt(2*pi))).*exp(-s_p.^2/(sigma^2*2)); % generate single Gauss
+
+  % Always normalize to 1
   out_pulse = out_pulse .* (1./max(abs(out_pulse)));
 
-%  out_pulse_2=-(s_p./(1*sqrt(2*pi))).*exp(-s_p.^2/2); % 1st derivate Gauss
-%  out_pulse_3=-(1-s_p.^2)./(1*sqrt(2*pi)).*exp(-s_p.^2/2); % 2nd derivate Gauss
-
-%  if(dbg_level == 2)
-%
-%%    plot(s_p, out_pulse_2, '-g');
-%%    plot(s_p, out_pulse_3, '-r');
-%%    plot(s_p_2, out_pulse_4, '-m');
-%%    plot(s_p_2, out_pulse_5, '-c');
-%  end
 
--- uwb_pulse_analysis.m
+++ uwb_pulse_analysis.m
@@ -1,4 +1,30 @@
-function uwb_pulse_analysis()
+%function uwb_pulse_analysis()
 
+f_s=50e9;
+p_len=[0.3:0.3:3.3]*10e-9; % Pulse lengths
+p_type=[ 0, 1, 2 ];    % Gauss, 1st Gauss derivate, Sine
+
+%% Put it always to longest vector
+gauss_p=zeros(length(p_len), round(p_len(end)*2*f_s)); % *2 is used in uwb_gen_pulse for pulse window
+gauss_f=zeros(length(p_len), round(p_len(end)*2*f_s)); % *2 is used in uwb_gen_pulse for pulse window
+t_p=(-length(gauss_p)/2:+length(gauss_p)/2-1)./f_s;
+leg_str=[];
+for ixx=1:length(p_len)-1
+  o_p=uwb_gen_pulse(0, p_len(ixx), f_s);
+  %% Stick it to gauss_p structure (in the middle)
+  gauss_p(ixx,end/2-length(o_p)/2+1:end/2+length(o_p)/2)=o_p;
+
+  leg_str=[leg_str; num2str(p_len(ixx), '%0.2e')];
+
+  gauss_f(ixx, :)=20*log10(abs(fft(gauss_p(ixx, :))));
+end
+
+figure();
+plot(t_p, gauss_p');
+legend(leg_str);
+
+figure();
+plot(t_p, gauss_f');
+legend(leg_str);
 
 %endfunction
